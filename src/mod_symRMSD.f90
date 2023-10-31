7495490fbf77ec9bd042b70726dc88ab49313453
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-10-31 12:43:27 +0900

    [update] enable unittest

--- src/mod_symRMSD.f90
+++ src/mod_symRMSD.f90
@@ -1,3 +1,15 @@
 module mod_symRMSD
+  use mod_unittest
   implicit none
+!
+contains
+!
+  subroutine X()
+  type(unittest)               :: u
+  logical,parameter            :: T=.TRUE., F=.FALSE.
+  call u%init( 'test_unittest' )
+  call u%assert(                T,           'assert               bool    0  ' )
+  call u%finish_and_terminate()
+  end subroutine
+!
 end module mod_symRMSD
