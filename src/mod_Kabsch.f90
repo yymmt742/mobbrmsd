6492257301baf8fb85f462babffe2314b44e51ad
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-11-09 09:13:21 +0900

    [update] tmp

--- src/mod_Kabsch.f90
+++ src/mod_Kabsch.f90
@@ -54,77 +54,75 @@ contains
     !! work array, must be larger than Kabsch_worksize(d)
     !! if row_major, must be larger than Kabsch_worksize(n)
     logical, intent(in), optional :: row_major
-    integer(IK)                   :: d1, dd, s, u, vt, iw
+!   integer(IK)                   :: d1, dd, s, u, vt, iw
     real(RK)                      :: St(n, n)
+    real(RK)                      :: s(d), u(d, d), vt(d, d), m(d, d)
     integer(IK)                   :: i,j
 !
     if (d < 1 .or. n < 1) RETURN
 !
-    if (optarg(row_major)) then
+!   if (optarg(row_major)) then
 !
-      w(:n * n) = [MATMUL(TRANSPOSE(RESHAPE(x(:d * n), [d, n])), RESHAPE(y(:d * n), [d, n]))]
+!     w(:n * n) = [MATMUL(TRANSPOSE(RESHAPE(x(:d * n), [d, n])), RESHAPE(y(:d * n), [d, n]))]
 !     call cov_row_major(d, n, x, y, w)
-      d1 = n
-      dd = d1 * d1
-      u = dd + 1
-      vt = u + dd
-      s = vt + dd
-      iw = s + d1
+!     d1 = n
+!     dd = d1 * d1
+!     u = dd + 1
+!     vt = u + dd
+!     s = vt + dd
+!     iw = s + d1
 !
-print*,d1,dd
-print'(5f9.3)', w(:dd)
-print*
-      call svd(d1, w, w(s), w(u), w(vt), w(iw))
+!     call svd(d1, w, w(s), w(u), w(vt), w(iw))
 !
-do concurrent(i=1:n,j=1:n)
-St(i, j) = MERGE(w(s + i - 1), 0D0, i == j)
-enddo
-print'(5f9.3)', [MATMUL(MATMUL(RESHAPE(w(u:u + dd - 1), [d1, d1]), St), RESHAPE(w(vt:vt + dd - 1),[d1,d1]))] - &
-              & [MATMUL(TRANSPOSE(RESHAPE(x(:n * n), [d, n])), RESHAPE(x(:n * n), [d, n]))]
-print*
-      call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
-print'(5f9.3)', w(:dd)
-print*
-      call det_sign(d1, w)
-print'(5f9.3)', w(1)
-print*
+!     call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
+!     call det_sign(d1, w)
 !
-      if (w(1) < ZERO) w(u + dd - d1:u + dd - 1) = -w(u + dd - d1:u + dd - 1)
-      call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
-      !rot(:dd) = [TRANSPOSE(RESHAPE(w(:dd), [d1, d1]))]
-print'(5f9.3)', w(:dd)
-print*
-      rot(:dd) = w(:dd)
+!     if (w(1) < ZERO) w(u + dd - d1:u + dd - 1) = -w(u + dd - d1:u + dd - 1)
+!     call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
+!     !rot(:dd) = [TRANSPOSE(RESHAPE(w(:dd), [d1, d1]))]
+!     rot(:dd) = w(:dd)
 !
-    else
+!   else
 !
-      d1 = d
-      dd = d1 * d1
-      u = dd + 1
-      vt = u + dd
-      s = vt + dd
-      iw = s + d1
+!     d1 = d
+!     dd = d1 * d1
+!     u = dd + 1
+!     vt = u + dd
+!     s = vt + dd
+!     iw = s + d1
 !
-      call cov(d, n, x, y, w)
-print'(6f9.3)', w(:dd)
-print*
+print*,d,n
+      call cov(d, n, x, y, m)
+      !call cov(d1, n, x, y, w)
 !
-      call svd(d1, w, w(s), w(u), w(vt), w(iw))
+      call svd(d, m, s, u, vt, w)
+      !call svd(d1, w, w(s), w(u), w(vt), w(iw))
 !
-      call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
-      w(:dd) = w(u:u+dd-1)
-      call det_sign(d1, w)
-      w(s) = w(1)
-      w(:dd) = w(vt:vt+dd-1)
-      call det_sign(d1, w)
-      w(s) = w(s) * w(1)
-print*,w(s)
+!     call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
+!     w(:dd) = w(u:u+dd-1)
 !
-      if (w(s) < ZERO) w(u + dd - d1:u + dd - 1) = -w(u + dd - d1:u + dd - 1)
-      call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
-      rot(:dd) = w(:dd)
+      w(:d * d) = [u]
+      call det_sign(d, w)
+      w(d * d + 1) = w(1)
+      w(:d * d) = [vt]
+      call det_sign(d, w)
+      w(1) = w(d * d + 1) * w(1)
+      print*,w(1)
+      if (w(1) < ZERO) u(:, d) = -u(:, d)
+      print'(6f9.3)',MATMUL(vt, u)
+      rot(:d * d) = [MATMUL(vt, u)]
+      print'(6f9.3)',rot(:d*d)
+!     call det_sign(d1, w)
+!     w(s) = w(1)
+!     w(:dd) = w(vt:vt+dd-1)
+!     call det_sign(d1, w)
+!     w(s) = w(s) * w(1)
 !
-    end if
+!     if (w(s) < ZERO) w(u + dd - d1:u + dd - 1) = -w(u + dd - d1:u + dd - 1)
+!     call DGEMM('N', 'N', d1, d1, d1, ONE, w(u), d1, w(vt), d1, ZERO, w, d1)
+!     rot(:dd) = w(:dd)
+!
+!   end if
 !
   end subroutine Kabsch
 !
