8fb67f236f0832a034ec9198947fd3b7c7db4434
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-11-14 10:22:03 +0900

    [update] mod_params.f90, add variable kind definition.

--- src/mod_params.f90
+++ src/mod_params.f90
@@ -3,17 +3,26 @@ module mod_params
     &                STDIN => INPUT_UNIT,  &
     &                STDOUT => OUTPUT_UNIT, &
     &                STDERR => ERROR_UNIT,  &
-    &                RK => REAL64,  &
-    &                IK => INT32
+    &                R4 => REAL32,  &
+    &                R8 => REAL64,  &
+    &                RQ => REAL128, &
+    &                I1 => INT8, &
+    &                I2 => INT16, &
+    &                I4 => INT32, &
+    &                I8 => INT64
   implicit none
   private
+  public  :: R4, I8, RQ
+  public  :: I1, I2, I4, I8
   public  :: RK, IK, LK
   public  :: STDIN, STDOUT, STDERR
   public  :: RZERO, RONE, RHALF
 !
 !&<
 !
-  integer(IK), parameter      :: LK = KIND(.true.)
+  integer, parameter          :: IK = I4
+  integer, parameter          :: RK = R8
+  integer, parameter          :: LK = KIND(.true.)
 !
   real(RK), parameter         :: RZERO = 0.0_RK
   real(RK), parameter         :: RONE  = 1.0_RK
