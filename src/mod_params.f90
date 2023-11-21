4dac5c28e6cb94950a90db7b2ed159fa7a6e6626
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-11-21 17:40:00 +0900

    [update] mod_params.f90

--- src/mod_params.f90
+++ src/mod_params.f90
@@ -16,7 +16,7 @@ module mod_params
   public  :: I1, I2, I4, I8
   public  :: RK, IK, LK
   public  :: STDIN, STDOUT, STDERR
-  public  :: RZERO, RONE, RHALF, RHUGE
+  public  :: RZERO, RONE, RHALF, RFOUR, RHUGE
 !
 !&<
 !
@@ -27,6 +27,7 @@ module mod_params
   real(RK), parameter         :: RZERO = 0.0_RK
   real(RK), parameter         :: RONE  = 1.0_RK
   real(RK), parameter         :: RHALF = 0.5_RK
+  real(RK), parameter         :: RFOUR = 4.0_RK
 !
   real(RK), parameter         :: RHUGE = HUGE(RZERO)
 !
