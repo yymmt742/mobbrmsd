a27f9f0f40208142d4c87f072bbb854b3fe19471
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-11-27 14:30:54 +0900

    [update] enable character parameters

--- src/mod_params.f90
+++ src/mod_params.f90
@@ -17,6 +17,10 @@ module mod_params
   public  :: RK, IK, LK
   public  :: STDIN, STDOUT, STDERR
   public  :: RZERO, RONE, RHALF, RFOUR, RHUGE
+  public  :: NEWLINE, CNULL, CARRET, ESCSEQ
+  public  :: FS_BOLD, FS_WEAK, FS_UNDER_LINE, FS_INVERT, FS_CROSSED_OUT, FS_RESET
+  public  :: FC_BLACK, FC_RED, FC_GREEN, FC_YELLOW
+  public  :: FC_MAGENTA, FC_CYAN, FC_WHITE
 !
 !&<
 !
@@ -30,6 +34,27 @@ module mod_params
   real(RK), parameter         :: RFOUR = 4.0_RK
 !
   real(RK), parameter         :: RHUGE = HUGE(RZERO)
+!
+  character(*), parameter     :: NEWLINE        = NEW_LINE(' ')
+  character(*), parameter     :: CNULL          = CHAR(0)
+  character(*), parameter     :: CARRET         = CHAR(13)
+  character(*), parameter     :: ESCSEQ         = CHAR(27)
+!
+  character(*), parameter     :: FS_BOLD        = ESCSEQ//'[1m'
+  character(*), parameter     :: FS_WEAK        = ESCSEQ//'[2m'
+  character(*), parameter     :: FS_UNDER_LINE  = ESCSEQ//'[4m'
+  character(*), parameter     :: FS_INVERT      = ESCSEQ//'[7m'
+  character(*), parameter     :: FS_CROSSED_OUT = ESCSEQ//'[9m'
+  character(*), parameter     :: FS_RESET       = ESCSEQ//'[0m'
+!
+  character(*), parameter     :: FC_BLACK       = ESCSEQ//'[30m'
+  character(*), parameter     :: FC_RED         = ESCSEQ//'[31m'
+  character(*), parameter     :: FC_GREEN       = ESCSEQ//'[32m'
+  character(*), parameter     :: FC_YELLOW      = ESCSEQ//'[33m'
+  character(*), parameter     :: FC_BLUE        = ESCSEQ//'[34m'
+  character(*), parameter     :: FC_MAGENTA     = ESCSEQ//'[35m'
+  character(*), parameter     :: FC_CYAN        = ESCSEQ//'[36m'
+  character(*), parameter     :: FC_WHITE       = ESCSEQ//'[37m'
 !
 !&>
 !
