b0935c06f85f873f6da80aa08f4204440190b747
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-12-04 12:23:54 +0900

    [update] mod_molecular_permutation.f90, enable n_free, n_mol

--- src/mod_molecular_permutation.f90
+++ src/mod_molecular_permutation.f90
@@ -8,8 +8,13 @@ module mod_molecular_permutation
 !
   type, extends(group_permutation) :: molecular_permutation
     private
+    integer(IK)              :: g = 0
+    integer(IK), allocatable :: iper(:)
     integer(IK), allocatable :: isym(:)
   contains
+    procedure :: n_mol    => molecular_permutation_n_mol
+    procedure :: n_free   => molecular_permutation_n_free
+    procedure :: child    => molecular_permutation_child
     procedure :: mol_swap => molecular_permutation_mol_swap
     final     :: molecular_permutation_destroy
   end type molecular_permutation
@@ -21,53 +26,93 @@ module mod_molecular_permutation
 contains
 !
 !| Constructer
-  pure function molecular_permutation_new(n, g, f, p, q) result(res)
+  pure elemental function molecular_permutation_new(n) result(res)
     integer(IK), intent(in)           :: n
     !! number of molecules
-    integer(IK), intent(in)           :: g
-    !! number of free molecules, must be g<=n.
-    integer(IK), intent(in), optional :: f(n)
-    !! injection n to g
-    integer(IK), intent(in), optional :: p(g)
-    !! f : injection n to g
-    integer(IK), intent(in), optional :: q(g)
-    !! f : injection n to g
-    integer(IK)                       :: prm(n)
     type(molecular_permutation)       :: res
-    integer(IK)                       :: i, j
 !
-    if (0 < n .and. 0 < g .and. n >= g &
-   &    .and. is_injection(n, g, f) &
-   &    .and. is_injection(g, g, p)) then
-      do concurrent(i=1:g)
-        prm(p(f(i))) = i
-      end do
-      j = g + 1
-      do i = 1, n
-        if (prm(i) > 0) cycle
-        prm(i) = j
-        j = j + 1
-      end do
+    if (n < 0) then
+      res%g = 0
+      ALLOCATE(res%iper(0))
+      ALLOCATE(res%isym(0))
     else
-      do concurrent(i=1:n)
-        prm(i) = i
-      end do
-    end if
+      res%g = n
+      ALLOCATE(res%iper(n))
+      ALLOCATE(res%isym(n), source=0)
+    endif
 !
-    call group_permutation_init(res, prm)
-!
-    if (PRESENT(q)) then
-      res%isym = q
-    else
-      allocate (res%isym(g), source=0)
-    end if
+    call group_permutation_init(res, res%iper)
 !
   end function molecular_permutation_new
 !
-  pure subroutine molecular_permutation_mol_swap(this, rot, d, m, X)
+  pure elemental function molecular_permutation_n_mol(this) result(res)
+    class(molecular_permutation), intent(in) :: this
+    integer(IK)                              :: res
+    if (ALLOCATED(this%iper)) then; res = SIZE(this%iper)
+    else; res = 0
+    end if
+  end function molecular_permutation_n_mol
+!
+  pure elemental function molecular_permutation_n_free(this) result(res)
+    class(molecular_permutation), intent(in) :: this
+    integer(IK)                              :: res
+    res = this%g
+  end function molecular_permutation_n_free
+!
+  pure elemental function molecular_permutation_child(this, iper, isym) result(res)
+    class(molecular_permutation), intent(in) :: this
+    integer(IK), intent(in)                  :: iper, isym
+    type(molecular_permutation)              :: res
+    integer(IK)                              :: i, n
+!
+    n = this%n_mol()
+!
+    allocate (res%iper(n))
+    allocate (res%isym(n), source=0)
+!
+    if (.not. ALLOCATED(this%iper)) then
+      res%g = 0
+      do concurrent(i=1:n)
+        res%iper(i) = i
+      enddo
+    elseif (this%g < 0 .or. iper < 1 .or. this%g < iper) then
+      res%g = 0
+      do concurrent(i=1:n)
+        res%iper(i) = this%iper(i)
+      enddo
+    else
+      res%g = this%g - 1
+      do concurrent(i=1:iper-1)
+        res%iper(i) = this%iper(i)
+      enddo
+      do concurrent(i=iper+1:this%g)
+        res%iper(i - 1) = this%iper(i)
+      enddo
+      res%iper(this%g) = this%iper(iper)
+      do concurrent(i=this%g+1:n)
+        res%iper(i - 1) = this%iper(i)
+      enddo
+    end if
+!
+    if (.not. ALLOCATED(this%iper)) then
+    else
+      do concurrent(i=1:this%g-1)
+        res%isym(i) = this%isym(i)
+      enddo
+      res%isym(this%g) = isym
+      do concurrent(i=this%g+1:n)
+        res%isym(i) = this%isym(i)
+      enddo
+    endif
+!
+    call group_permutation_init(res, res%iper)
+!
+  end function molecular_permutation_child
+!
+  pure subroutine molecular_permutation_mol_swap(this, d, m, rot, X)
     class(molecular_permutation), intent(inout) :: this
-    type(molecular_rotation), intent(in)        :: rot
     integer(IK), intent(in)                     :: d, m
+    type(molecular_rotation), intent(in)        :: rot
     real(RK), intent(inout)                     :: X(*)
     integer(IK)                                 :: i, dm
     dm = d * m
@@ -83,26 +128,28 @@ contains
 !
   pure elemental subroutine molecular_permutation_destroy(this)
     type(molecular_permutation), intent(inout) :: this
+    this%g = 0
+    if (ALLOCATED(this%iper)) deallocate (this%iper)
     if (ALLOCATED(this%isym)) deallocate (this%isym)
     call this%clear()
   end subroutine molecular_permutation_destroy
 !
 !!!
 !
-  pure function is_injection(n, g, f) result(res)
-    integer(IK), intent(in)           :: n, g
-    integer(IK), intent(in), optional :: f(g)
-    logical                           :: res
-    integer(IK)                       :: i
-    res = .FALSE.
-    if (.not. PRESENT(f)) return
-    do i = 1, g
-      if (f(i) < 1 .or. n < f(i)) return
-    end do
-    do i = 1, g - 1
-      if (ANY(f(i) == f(i + 1:))) return
-    end do
-    res = .TRUE.
-  end function is_injection
+! pure function is_injection(n, g, f) result(res)
+!   integer(IK), intent(in)           :: n, g
+!   integer(IK), intent(in), optional :: f(g)
+!   logical                           :: res
+!   integer(IK)                       :: i
+!   res = .FALSE.
+!   if (.not. PRESENT(f)) return
+!   do i = 1, g
+!     if (f(i) < 1 .or. n < f(i)) return
+!   end do
+!   do i = 1, g - 1
+!     if (ANY(f(i) == f(i + 1:))) return
+!   end do
+!   res = .TRUE.
+! end function is_injection
 !
 end module mod_molecular_permutation
