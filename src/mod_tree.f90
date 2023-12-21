3a1c7013c5503904753df6952627c3fc485e881f
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2023-12-22 01:19:21 +0900

    [update] mod_tree.f90

--- src/mod_tree.f90
+++ src/mod_tree.f90
@@ -6,7 +6,7 @@ module mod_tree
   use mod_partial_rmsd
   implicit none
   private
-  public :: d_matrix_list, node, breadth
+  public :: node, breadth
 !
   type node
     private
@@ -15,14 +15,14 @@ module mod_tree
     integer(IK), allocatable :: per(:)
     real(RK)                 :: H
     real(RK), allocatable    :: C(:), L(:)
-    !! molecular permutation block
-    real(RK), public         :: lowerbound = RHUGE
+    real(RK), public         :: lowerbound = ZERO
     !! the lower bound
   contains
     procedure         :: n_per            => node_n_per
     procedure         :: n_sym            => node_n_sym
-!   procedure         :: generate_breadth => node_generate_breadth
-!   procedure         :: has_child        => node_has_child
+    procedure         :: n_depth          => node_n_depth
+    procedure         :: generate_breadth => node_generate_breadth
+    procedure         :: has_child        => node_has_child
     final             :: node_destroy
   end type node
 !
@@ -75,47 +75,6 @@ contains
     res%L = res%L(res%ispc + 1:)
 !
   end function node_new_as_root
-!
-  pure subroutine next_index(l, m, depth, ispc, child)
-    integer(IK), intent(in)    :: l, depth, ispc
-    type(d_matrix), intent(in) :: m(l)
-    type(node), intent(inout)  :: child
-    integer(IK)                :: i, spc
-    spc = MAX(ispc, 1)
-    if (depth < m(spc)%g) then
-      child%ispc = spc
-      child%depth = depth + 1
-      return
-    end if
-    do i = spc + 1, l
-      if (m(i)%g < 1) cycle
-      child%ispc = i
-      child%depth = 1
-      return
-    end do
-  end subroutine next_index
-!
-  pure elemental function node_n_sym(this, dmat) result(res)
-    class(node), intent(in)         :: this
-    type(d_matrix_list), intent(in) :: dmat
-    integer(IK)                     :: res
-    if(this%ispc<1.or.dmat%l<this%ispc)then
-      res = 0
-    else
-      res = dmat%m(this%ispc)%s
-    endif
-  end function node_n_sym
-!
-  pure elemental function node_n_per(this, dmat) result(res)
-    class(node), intent(in)         :: this
-    type(d_matrix_list), intent(in) :: dmat
-    integer(IK)                     :: res
-    if (this%ispc < 1 .or. dmat%l < this%ispc) then
-      res = 0
-    else
-      res = dmat%m(this%ispc)%g - this%depth + 1
-    end if
-  end function node_n_per
 !
   pure function node_new(parent, iper, isym, dmat, W) result(res)
     type(node), intent(in)          :: parent
@@ -180,139 +139,91 @@ contains
 !
   end function node_new
 !
-! pure elemental function node_has_child(this) result(res)
-!   class(node), intent(in) :: this
-!   logical                 :: res
-!   res = this%blk%has_child()
-! end function node_has_child
+  pure subroutine next_index(l, m, depth, ispc, child)
+    integer(IK), intent(in)    :: l, depth, ispc
+    type(d_matrix), intent(in) :: m(l)
+    type(node), intent(inout)  :: child
+    integer(IK)                :: i, spc
+    spc = MAX(ispc, 1)
+    if (depth < m(spc)%g) then
+      child%ispc = spc
+      child%depth = depth + 1
+      return
+    end if
+    do i = spc + 1, l
+      if (m(i)%g < 1) cycle
+      child%ispc = i
+      child%depth = 1
+      return
+    end do
+  end subroutine next_index
 !
-! pure subroutine proc_fixed_part(blk, X, Y, prd)
-!   class(mol_block_list), intent(in) :: blk
-!   real(RK), intent(in)              :: X(*)
-!   real(RK), intent(in)              :: Y(*)
-!   type(partial_rmsd), intent(inout) :: prd
-!   real(RK)                          :: W(nwork1(blk))
-!   integer(IK)                       :: r(blk%nspecies())
-!   integer(IK)                       :: p(blk%nspecies())
-!   integer(IK)                       :: q(blk%nspecies())
-!   integer(IK)                       :: i, ix, iy, s, n
+  pure elemental function node_n_depth(this, dmat) result(res)
+    class(node), intent(in)         :: this
+    type(d_matrix_list), intent(in) :: dmat
+    integer(IK)                     :: res
+    if(this%ispc<1.or.dmat%l<this%ispc)then
+      res = 0
+    else
+      res = SUM(dmat%m(this%ispc:)%g) - this%depth + 1
+    endif
+  end function node_n_depth
 !
-!   s = blk%nspecies()
+  pure elemental function node_n_sym(this, dmat) result(res)
+    class(node), intent(in)         :: this
+    type(d_matrix_list), intent(in) :: dmat
+    integer(IK)                     :: res
+    if(this%ispc<1.or.dmat%l<this%ispc)then
+      res = 0
+    else
+      res = dmat%m(this%ispc)%s
+    endif
+  end function node_n_sym
 !
-!   do i = 1, s
-!     p(i) = blk%res_pointer(i)
-!   end do
+  pure elemental function node_n_per(this, dmat) result(res)
+    class(node), intent(in)         :: this
+    type(d_matrix_list), intent(in) :: dmat
+    integer(IK)                     :: res
+    if (this%ispc < 1 .or. dmat%l < this%ispc) then
+      res = 0
+    else
+      res = dmat%m(this%ispc)%g - this%depth + 1
+    end if
+  end function node_n_per
 !
-!   r = blk%n_res()
-!   n = SUM(r)
-!   r = blk%d * r
-!
-!   q(1) = 0
-!   do i = 1, s - 1
-!     q(i + 1) = r(i) + q(i)
-!   end do
-!
-!   ix = 1
-!   iy = ix + blk%d * n
-!
-!   do concurrent(i = 1:s)
-!     if (r(i) > 0) call copy(r(i), X(p(i)), w(ix + q(i)))
-!   end do
-!   do concurrent(i = 1:s)
-!     if (r(i) > 0) call copy(r(i), Y(p(i)), w(iy + q(i)))
-!   end do
-!   prd = partial_rmsd(blk%d, n, W(ix:), W(iy:))
-!
-! end subroutine proc_fixed_part
-!
-! pure elemental function nwork1(blk) result(res)
-!   class(mol_block_list), intent(in) :: blk
-!   integer(IK)                       :: res
-!   res = MAX(1, 2 * SUM(blk%n_res()) * blk%d)
-! end function nwork1
-!
-! pure subroutine copy(n, x, w)
-!   integer(IK), intent(in) :: n
-!   real(RK), intent(in)    :: x(*)
-!   real(RK), intent(inout) :: w(*)
-!   integer(IK)             :: i
-!   do concurrent(i=1:n)
-!     w(i) = x(i)
-!   end do
-! end subroutine copy
+  pure elemental function node_has_child(this) result(res)
+    class(node), intent(in) :: this
+    logical                 :: res
+    res = this%ispc > 0
+  end function node_has_child
 !
 !| generate childe nodes instance
-! function node_generate_breadth(this, rot, x, y) result(res)
-!   class(node), intent(in)               :: this
-!   !! molecular template
-!   class(molecular_rotation), intent(in) :: rot(:)
-!   !! molecular template
-!   real(RK), intent(in)                  :: x(*)
-!   !! reference molecular coordinate, x(d,m,n)
-!   real(RK), intent(in)                  :: y(*)
-!   !! target molecular coordinate, y(d,m,n)
-!   type(breadth)                         :: res
-!   type(mol_block_list)                  :: b_child
-!   integer(IK), allocatable              :: per(:)
-!   integer(IK)                           :: px, bm, bs
-!   integer(IK)                           :: iper, ipnt, isym, ispc
-!   integer(IK)                           :: nper, nsym
+  pure function node_generate_breadth(this, dmat, W) result(res)
+    class(node), intent(in)         :: this
+    type(d_matrix_list), intent(in) :: dmat
+    real(RK), intent(in)            :: W(*)
+    type(breadth)                   :: res
+    integer(IK)                     :: nper, nsym, iper, isym
 !
-!   if (.not. this%has_child()) then
-!     allocate (res%nodes(0))
-!     return
-!   end if
+    if (.not. this%has_child()) then
+      allocate (res%nodes(0))
+      return
+    end if
 !
-!   b_child = this%blk%child()
-!   ispc = this%blk%ispecies()
+    nper = this%n_per(dmat)
+    nsym = this%n_sym(dmat)
 !
-!   ipnt = this%blk%ipointer(0)
-!   nper = this%blk%b(ispc)%g
-!   nsym = rot(ispc)%n_sym()
+    allocate (res%nodes(nper * nsym))
 !
-!   bm = this%blk%b(ispc)%m
-!   bs = this%blk%d * bm
-!   px = ipnt + SIZE(this%per) * bs
+    do concurrent(iper=1:nper, isym=1:nsym)
+      block
+        integer(IK) :: inod
+        inod = (isym - 1) * nper + iper
+        res%nodes(inod) = node_new(this, iper, isym, dmat, W)
+      end block
+    end do
 !
-!   per = resper(nper, nper + SIZE(this%per), this%per)
-!   allocate (res%nodes(nper * (nsym + 1)))
-!
-!print*,ipnt, nper, nsym, bm, bs, px
-!rint*, per
-!
-!   do concurrent(iper=1:nper, isym=0:nsym)
-!     block
-!       integer(IK) :: py, inod
-!       real(RK)    :: w(bs, 2)
-!       inod = isym * nper + iper
-!       res%nodes(inod)%blk = b_child
-!       if (nper == 1) then
-!         allocate (res%nodes(inod)%per(0))
-!       else
-!         res%nodes(inod)%per = [this%per, per(iper)]
-!       end if
-!       py = ipnt + (per(iper) - 1) * bs
-!       call copy(bs, X(px), w(1, 1))
-!       call copy(bs, Y(py), w(1, 2))
-!       call rot(ispc)%swap(b_child%d, w(1, 2), isym)
-!       res%nodes(inod)%prd = this%prd%append(bm, w(:, 1), w(:, 2))
-!       res%nodes(inod)%lowerbound = res%nodes(inod)%prd%sd()
-!     end block
-!   end do
-!
-! end function node_generate_breadth
-!
-! pure function resper(g, n, per) result(res)
-!   integer(IK), intent(in) :: g, n, per(n)
-!   integer(IK)             :: i, j, res(g)
-!   j = 0
-!   do i = 1, n
-!     if (ANY(i == per)) cycle
-!     j = j + 1
-!     res(j) = i
-!     if (j == g) return
-!   end do
-! end function resper
+  end function node_generate_breadth
 !
   pure elemental subroutine node_destroy(this)
     type(node), intent(inout) :: this
--- src/test/test_tree.f90
+++ src/test/test_tree.f90
@@ -10,7 +10,7 @@ program main
 !
   call u%init('test node')
   call test1()
-! call test2()
+  call test2()
 ! call test3()
 !
   call u%finish_and_terminate()
@@ -33,8 +33,7 @@ contains
     real(RK), allocatable       :: W(:)
     type(d_matrix_list)         :: dmat
     type(node)                  :: a1, a2, a3, a4, a5, a6, a7, a8
-    !type(breadth)               :: z1, z2, z3, z4, z5, z6, z7, z8
-    integer                     :: i, ml
+    integer                     :: i
 !
     rot(1) = molecular_rotation(RESHAPE([2, 3, 1, 4, 5, 3, 1, 2, 4, 5], [m1, 2]))
     rot(2) = molecular_rotation(RESHAPE([(i, i=1,0)], [0, 1]))
@@ -81,6 +80,67 @@ contains
     print'(*(f16.3))',a8%lowerbound
 !
   end subroutine test1
+!
+  subroutine test2()
+    integer, parameter          :: d = 3
+    integer, parameter          :: s = 3
+    integer, parameter          :: m1 = 5, n1 = 3, f1 = 3, g1 = 2
+    integer, parameter          :: m2 = 3, n2 = 4, f2 = 2, g2 = 2
+    integer, parameter          :: m3 = 7, n3 = 5, f3 = 7, g3 = 3
+    integer, parameter          :: mn = m1 * n1 + m2 * n2 + m3 * n3
+    type(mol_block_list)        :: blk
+    type(mol_block)             :: b(3) = [mol_block(0, 3, m1, n1, f1, g1), &
+                                        &  mol_block(0, 1, m2, n2, f2, g2), &
+                                        &  mol_block(0, 2, m3, n3, f3, g3)]
+    type(molecular_rotation)    :: rot(s)
+    real(RK)                    :: X(d, mn), Y(d, mn)
+    real(RK), allocatable       :: W(:)
+    type(d_matrix_list)         :: dmat
+    type(node)                  :: a
+    type(breadth)               :: z1, z2, z3, z4, z5, z6, z7, z8
+    integer                     :: i, ml
+!
+    rot(1) = molecular_rotation(RESHAPE([2, 3, 1, 4, 5, 3, 1, 2, 4, 5], [m1, 2]))
+    rot(2) = molecular_rotation(RESHAPE([(i, i=1,0)], [0, 1]))
+    rot(3) = molecular_rotation(RESHAPE([7, 6, 5, 4, 3, 2, 1], [m3, 1]))
+    blk = mol_block_list(d, s, b)
+    dmat = d_matrix_list(blk, 1)
+    allocate(W(dmat%memsize()))
+!
+    X = sample(d,mn)
+    Y = 0.9D0 * X + 0.1D0 * sample(d, mn)
+!
+    call dmat%eval(rot, X, Y, W)
+!
+    a = node(dmat, W)
+print'(6i4)',b
+print*,a%n_depth(dmat)
+    z1 = a%generate_breadth(dmat, W)
+    ml = MINLOC(z1%nodes%lowerbound, 1)
+    z2 = z1%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z2%nodes%lowerbound, 1)
+    z3 = z2%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z3%nodes%lowerbound, 1)
+    z4 = z3%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z4%nodes%lowerbound, 1)
+    z5 = z4%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z5%nodes%lowerbound, 1)
+    z6 = z5%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z6%nodes%lowerbound, 1)
+    z7 = z6%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z7%nodes%lowerbound, 1)
+    z8 = z7%nodes(ml)%generate_breadth(dmat, W)
+    ml = MINLOC(z8%nodes%lowerbound, 1)
+    print'(8(f9.3))',z1%nodes%lowerbound
+    print'(8(f9.3))',z2%nodes%lowerbound
+    print'(8(f9.3))',z3%nodes%lowerbound
+    print'(8(f9.3))',z4%nodes%lowerbound
+    print'(8(f9.3))',z5%nodes%lowerbound
+    print'(8(f9.3))',z6%nodes%lowerbound
+    print'(8(f9.3))',z7%nodes%lowerbound
+    print'(8(f9.3))',z8%nodes%lowerbound
+!
+  end subroutine test2
 !
   function sample(d, n) result(res)
     integer, intent(in)  :: d, n
