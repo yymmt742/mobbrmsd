dc63f3c6f66715a4f2820afffaa0bd99a3d092f6
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-01-04 10:38:37 +0900

    [update] test_Hungarian.f90

--- src/symRMSD/test/test_Hungarian.f90
+++ src/symRMSD/test/test_Hungarian.f90
@@ -3,27 +3,28 @@ program main
   use mod_Hungarian
   use mod_unittest
   implicit none
-  type(unittest) :: z
+  type(unittest) :: u
+  integer(IK), parameter :: NTEST = 25
+  integer(IK)            :: itest
 !
-  call z%init('test Hungarian')
-  call test1()
-  call test2()
+  call u%init('test Hungarian')
+  do itest =1,NTEST
+    call test1()
+  enddo
+  do itest =1,NTEST
+    call test2()
+  enddo
 !
-  call z%finish_and_terminate()
+  call u%finish_and_terminate()
 !
 contains
 !
   subroutine test1()
-    integer, parameter :: N_TEST=10
-    integer, parameter :: d=3
-    integer, parameter :: n=3
-    integer, parameter :: i1(3) = [1,2,3]
-    integer, parameter :: i2(3) = [1,3,2]
-    integer, parameter :: i3(3) = [2,1,3]
-    integer, parameter :: i4(3) = [2,3,1]
-    integer, parameter :: i5(3) = [3,1,2]
-    integer, parameter :: i6(3) = [3,2,1]
+    integer(IK), parameter :: N_TEST=10
+    integer(IK), parameter :: d=3
+    integer(IK), parameter :: n=3
     real(RK)           :: X(d, n), Y(d, n), C(n, n), P(n, n)
+    real(RK)           :: minsp
     integer            :: piv(n), i, j
 !
     call random_number(X)
@@ -31,37 +32,32 @@ contains
     C = MATMUL(TRANSPOSE(X), Y)
     call Hungarian(n, C, P, piv)
     do concurrent(i=1:n, j=1:n)
-      P(i, j) = MERGE(ONE, ZERO, i == piv(j))
+      P(i, j) = MERGE(ONE, ZERO, piv(i) == j)
     end do
-    print'(3f9.3)', C,P
-    print'(3f9.3)', SUM(C * TRANSPOSE(P))
-    print'(3f9.3)', Hungarian_value(n, C)
-    print*
-    print'(3f9.3)', matmul(C, P)
-    print*
-    print'(3f9.3)', matmul(P, C)
-    print*
-    print'(3i4,f9.6)',i1, SP(n, i1, C)
-    print'(3i4,f9.6)',i2, SP(n, i2, C)
-    print'(3i4,f9.6)',i3, SP(n, i3, C)
-    print'(3i4,f9.6)',i4, SP(n, i4, C)
-    print'(3i4,f9.6)',i5, SP(n, i5, C)
-    print'(3i4,f9.6)',i6, SP(n, i6, C)
+    minsp = 999
+    minsp = MIN(minsp, SP(n, [1_IK, 2_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(n, [1_IK, 3_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(n, [2_IK, 1_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(n, [2_IK, 3_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(n, [3_IK, 1_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(n, [3_IK, 2_IK, 1_IK], C))
+    call u%assert_almost_equal(SUM(C * P), Hungarian_value(n, C), 'CP = X    ')
+    call u%assert_almost_equal(Hungarian_value(n, C), minsp,      'X  = minsp')
 !
   end subroutine test1
 !
   subroutine test2()
-    integer, parameter :: N_TEST=10
-    integer, parameter :: n=1000
+    integer, parameter :: N_TEST = 10
+    integer, parameter :: n = 1000
     real(RK)           :: C(n, n), P(n, n)
     integer            :: i, j, piv(n)
 !
-    call random_number(C)
+    call RANDOM_NUMBER(C)
     call Hungarian(n, C, P, piv)
     do concurrent(i=1:n, j=1:n)
-    P(i, j) = MERGE(ONE, ZERO, i == piv(j))
-  end do
-    print'(f9.3)', SUM(C * TRANSPOSE(P))
+      P(i, j) = MERGE(ONE, ZERO, piv(i) == j)
+    end do
+    call u%assert_almost_equal(SUM(C * P), Hungarian_value(n, C), 'CP = X    ')
 !
   end subroutine test2
 !
