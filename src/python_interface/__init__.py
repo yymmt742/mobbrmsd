e699eaeb9691fc5453cca0e79c26a5683aac4e6b
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-02-13 13:40:41 +0900

    [update] __init__.py

--- src/python_interface/__init__.py
+++ src/python_interface/__init__.py
@@ -1,1 +1,22 @@
 from .symrmsd import driver
+
+setup_done = False
+
+def add_molecule(m=1, n=1, s=1, swp=[0]):
+  driver.set_molecule(m, n, s, swp)
+  setup_done = False
+
+def run(x, y, maxeval=None, cutoff=None):
+  if maxeval != None:
+    driver.maxeval = maxeval
+    setup_done = False
+  if cutoff != None:
+    driver.cutoff = cutoff
+    setup_done = False
+  if setup_done == False:
+    driver.setup()
+  import numpy as np
+  x_ = np.array(x).flatten()
+  y_ = np.array(y).flatten()
+  n_ = y_.shape[0]/x_.shape[0]
+  return driver.run(x_, y_, n_)
