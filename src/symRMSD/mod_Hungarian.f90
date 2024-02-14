d47df66276e45c489125193a8ec410411c206ea2
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-02-14 18:10:33 +0900

    [update] mod_Hungarian.f90

--- src/symRMSD/mod_Hungarian.f90
+++ src/symRMSD/mod_Hungarian.f90
@@ -1,109 +1,74 @@
 module mod_Hungarian
-  use mod_params, only : IK, RK, ONE => RONE, ZERO => RZERO, RHUGE
+  use mod_params, only: IK, RK, ONE => RONE, ZERO => RZERO, RHUGE
   implicit none
   private
   public :: Hungarian, Hungarian_value
 !
 contains
 !
-  pure function Hungarian_value(n, C) result(res)
+!| Calculate linear assignment minimum cost using Hungarian method.
+  pure function Hungarian_value(m, n, C) result(res)
+    integer(IK), intent(in) :: m
+    !! matrix dimension 1, must be m > 0.
     integer(IK), intent(in) :: n
-    !! matrix dimension
+    !! matrix dimension 2, must be n > 0 and m <= n.
     real(RK), intent(in)    :: C(*)
-    !! n*n score matrix.
+    !! score matrix C(m, n).
     real(RK)                :: res
-    if (n < 1) then
+    if (m < 1) then
       res = ZERO
       return
-    elseif (n == 1) then
-      res = C(1)
-    elseif (n == 2) then
-      if (C(1) + C(4) >= C(2) + C(3)) then
-        res = C(1) + C(4)
-      else
-        res = C(2) + C(3)
-      end if
+    elseif (m > n) then
+      res = ZERO
+      return
+    elseif (m == 1) then
+      res = MINVAL(C(:n))
     else
       block
         real(RK)    :: W(n + n + 1)
-        integer(IK) :: iw(3 * (n + 1)), i, j
-!
-        call get_piv(n, n + 1, C, iw(1), iw(n + 2), iw(n + n + 3), W(1), W(n + 1))
-        res = ZERO
-        do i = 1, n
-          j = i + n * (iw(i) - 1)
-          res = res + C(j)
-        end do
+        integer(IK) :: iw(n + n + n + 3)
+        call get_piv(m, n, C, iw(1), iw(n + 2), iw(n + n + 3), W(1), W(n + 1), res)
       end block
     end if
   end function Hungarian_value
 !
-!| Hungarian method
-  pure  subroutine Hungarian(n, C, W, piv)
-    integer(IK), intent(in)              :: n
-    !! matrix dimension
-    real(RK), intent(in)                 :: C(*)
-    !! pivot index
-    real(RK), intent(inout)              :: W(*)
-    !! work array
-    integer(IK), intent(inout), optional :: piv(*)
-    !! n*n score matrix.
+!| Calculate linear assignment minimum cost using Hungarian method with pivot.
+  subroutine Hungarian(m, n, C, W)
+  !pure subroutine Hungarian(m, n, C, W, piv)
+    integer(IK), intent(in)    :: m
+    !! matrix dimension 1, must be m > 0.
+    integer(IK), intent(in)    :: n
+    !! matrix dimension 2, must be n > 0 and m <= n.
+    real(RK), intent(in)       :: C(*)
+    !! score matrix C(m, n).
+    real(RK), intent(inout)    :: W(*)
+    !! work array.
 !
     if (n < 0)then
       ! query work array size
-      W(1) = ABS(n + n) + 1
-    elseif (n == 0)then
+      W(1) = ABS(n + n) + 2
+    elseif (m < 1.or.m > n)then
       return
-    elseif (n == 1) then
-      W(1) = C(1)
-      if (PRESENT(piv)) piv(1) = 1
-    elseif (n == 2) then
-      W(1) = C(1) + C(4)
-      W(2) = C(2) + C(3)
-      if (W(1) >= W(2))then
-        if (PRESENT(piv)) then
-          piv(1) = 1
-          piv(2) = 2
-        end if
-      else
-        W(2) = W(1)
-        if (PRESENT(piv)) then
-          piv(1) = 2
-          piv(2) = 1
-        end if
-      endif
+    elseif (m == 1) then
+      W(1) = MINVAL(C(:n))
     else
       block
-        integer(IK) :: iw(n + n + n + 3), n1, i, j
-!
-        n1 = n + 1
-        call get_piv(n, n1, C, iw(1), iw(n + 2), iw(n + n + 3), W(1), W(n + 1))
-!
-        W(1) = ZERO
-        do i = 1, n
-          j = i + n * (iw(i) - 1)
-          W(1) = W(1) + C(j)
-        end do
-!
-        if (PRESENT(piv)) then
-          do concurrent(i=1:n)
-            piv(i) = iw(i)
-          end do
-        end if
-!
+        integer(IK) :: iw(n + n + n + 3)
+        call get_piv(m, n, C, iw(1), iw(n + 2), iw(n + n + 3), W(2), W(n + 2), W(1))
       end block
     end if
 !
   end subroutine Hungarian
 !
-  pure subroutine get_piv(n, n1, C, piv, is_visited, prv, y, cij)
-    integer(IK), intent(in)    :: n, n1
-    real(RK), intent(in)       :: C(n, n)
-    integer(IK), intent(inout) :: piv(n1), is_visited(n1), prv(n1)
-    real(RK), intent(inout)    :: y(*), cij(*)
+  pure subroutine get_piv(m, n, C, piv, is_visited, prv, y, cij, res)
+    integer(IK), intent(in)    :: m, n
+    real(RK), intent(in)       :: C(m, n)
+    integer(IK), intent(inout) :: piv(*), is_visited(*), prv(*)
+    real(RK), intent(inout)    :: y(*), cij(*), res
     real(RK)                   :: minc, edge, cedg
-    integer(IK)                :: i, ic, ix, j
+    integer(IK)                :: n1, i, j, ic, ix
 !
+    n1 = n + 1
     do concurrent(i=1:n1)
       piv(i) = -1
     end do
@@ -112,7 +77,9 @@ contains
       y(i) = ZERO
     end do
 !
-    do j = 1, n
+    res = ZERO
+!
+    do j = 1, m
 !
       do concurrent(i=1:n1)
         is_visited(i) = 0
@@ -136,8 +103,8 @@ contains
         ix = -1
         do i = 1, n
           if (is_visited(i) == 0) then
-            edge = C(i, piv(ic)) - y(i)
-            if (ic < n1) edge = edge - C(ic, piv(ic)) + y(ic)
+            edge = C(piv(ic), i) - y(i)
+            if (ic < n1) edge = edge - C(piv(ic), ic) + y(ic)
             cedg = cij(ic) + edge
             if (cij(i) > cedg) then
               prv(i) = ic
@@ -158,6 +125,8 @@ contains
       do concurrent(i = 1:n)
         y(i) = y(i) + cij(i)
       end do
+!
+      res = res + y(ic)
 !
       do while(ic /= n1)
         i = prv(ic)
--- src/symRMSD/test/test_Hungarian.f90
+++ src/symRMSD/test/test_Hungarian.f90
@@ -7,10 +7,11 @@ program main
   integer(IK), parameter :: NTEST = 25
   integer(IK)            :: itest
 !
-  call u%init('test Hungarian')
+  call u%init('test Hungarian square')
   do itest =1,NTEST
     call test1()
   enddo
+  call u%init('test Hungarian general')
   do itest =1,NTEST
     call test2()
   enddo
@@ -23,51 +24,121 @@ contains
     integer(IK), parameter :: N_TEST=10
     integer(IK), parameter :: d=3
     integer(IK), parameter :: n=3
-    real(RK)           :: X(d, n), Y(d, n), C(n, n), P(n, n)
-    real(RK)           :: minsp
-    integer            :: piv(n), i, j
+    real(RK)               :: C(n, n), dum(1)
+    real(RK)               :: minsp
 !
-    call random_number(X)
-    call random_number(Y)
-    C = MATMUL(TRANSPOSE(X), Y)
-    call Hungarian(n, C, P, piv)
-    do concurrent(i=1:n, j=1:n)
-      P(i, j) = MERGE(ONE, ZERO, piv(i) == j)
-    end do
+    call random_number(C)
     minsp = 999
-    minsp = MIN(minsp, SP(n, [1_IK, 2_IK, 3_IK], C))
-    minsp = MIN(minsp, SP(n, [1_IK, 3_IK, 2_IK], C))
-    minsp = MIN(minsp, SP(n, [2_IK, 1_IK, 3_IK], C))
-    minsp = MIN(minsp, SP(n, [2_IK, 3_IK, 1_IK], C))
-    minsp = MIN(minsp, SP(n, [3_IK, 1_IK, 2_IK], C))
-    minsp = MIN(minsp, SP(n, [3_IK, 2_IK, 1_IK], C))
-    call u%assert_almost_equal(SUM(C * P), Hungarian_value(n, C), 'CP = X    ')
-    call u%assert_almost_equal(Hungarian_value(n, C), minsp,      'X  = minsp')
+    minsp = MIN(minsp, SP(n, n, [1_IK, 2_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(n, n, [1_IK, 3_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(n, n, [2_IK, 1_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(n, n, [2_IK, 3_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(n, n, [3_IK, 1_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(n, n, [3_IK, 2_IK, 1_IK], C))
+    call Hungarian(-n,-n, C, dum)
+    block
+      real(RK) :: W(NINT(dum(1)))
+      call Hungarian(n, n, C, W)
+      call u%assert_almost_equal(W(1), Hungarian_value(n, n, C),  'W1 = HV   ')
+      call u%assert_almost_equal(Hungarian_value(n, n, C), minsp, 'HV = minsp')
+    end block
 !
   end subroutine test1
 !
   subroutine test2()
-    integer, parameter :: N_TEST = 10
-    integer, parameter :: n = 1000
-    real(RK)           :: C(n, n), P(n, n)
-    integer            :: i, j, piv(n)
+    integer(IK), parameter :: N_TEST=10
+    integer(IK), parameter :: d=3
+    integer(IK), parameter :: m=3
+    integer(IK), parameter :: n=5
+    real(RK)               :: C(m, n), dum(1)
+    real(RK)               :: minsp
 !
-    call RANDOM_NUMBER(C)
-    call Hungarian(n, C, P, piv)
-    do concurrent(i=1:n, j=1:n)
-      P(i, j) = MERGE(ONE, ZERO, piv(i) == j)
-    end do
-    call u%assert_almost_equal(SUM(C * P), Hungarian_value(n, C), 'CP = X    ')
+    call random_number(C)
+    minsp = 999
+    minsp = MIN(minsp, SP(m, n, [1_IK, 2_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 2_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 2_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 3_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 3_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 4_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 3_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 3_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 4_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 4_IK, 5_IK], C))
+
+    minsp = MIN(minsp, SP(m, n, [1_IK, 3_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 4_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 5_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 4_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 5_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [1_IK, 5_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 4_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 5_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 5_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 5_IK, 4_IK], C))
+
+    minsp = MIN(minsp, SP(m, n, [2_IK, 1_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 1_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 1_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 1_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 1_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 1_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 2_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 2_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 2_IK, 5_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 3_IK, 5_IK], C))
+
+    minsp = MIN(minsp, SP(m, n, [2_IK, 3_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 4_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [2_IK, 5_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 4_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 5_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 5_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 4_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [3_IK, 5_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 5_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 5_IK, 3_IK], C))
+
+    minsp = MIN(minsp, SP(m, n, [3_IK, 1_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 1_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 1_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 1_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 1_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 1_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 2_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 2_IK, 3_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 2_IK, 4_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 3_IK, 4_IK], C))
+
+    minsp = MIN(minsp, SP(m, n, [3_IK, 2_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 2_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 2_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 3_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 3_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 4_IK, 1_IK], C))
+    minsp = MIN(minsp, SP(m, n, [4_IK, 3_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 3_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 4_IK, 2_IK], C))
+    minsp = MIN(minsp, SP(m, n, [5_IK, 4_IK, 3_IK], C))
+!
+    call Hungarian(m,-n, C, dum)
+    block
+      real(RK) :: W(NINT(dum(1)))
+      call Hungarian(m, n, C, W)
+      call u%assert_almost_equal(W(1), Hungarian_value(m, n, C),  'W1 = HV   ')
+      call u%assert_almost_equal(Hungarian_value(m, n, C), minsp, 'X  = minsp')
+    end block
+!
 !
   end subroutine test2
 !
-  pure function SP(n, ix, C) result(res)
-    integer, intent(in)  :: n, ix(n)
-    real(RK), intent(in) :: C(n, n)
+  pure function SP(m, n, ix, C) result(res)
+    integer, intent(in)  :: m, n, ix(n)
+    real(RK), intent(in) :: C(m, n)
     integer              :: i
     real(RK)             :: res
     res = 0D0
-    do i = 1, n
+    do i = 1, m
       res = res + C(i, ix(i))
     end do
   end function SP
