d510e6f2173f95eaff3b9d1691d24b8d99aca75a
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-03-10 18:02:33 +0900

    [update] wip

--- src/symRMSD/mod_f_matrix.f90
+++ src/symRMSD/mod_f_matrix.f90
@@ -10,102 +10,84 @@ module mod_f_matrix
   implicit none
   private
   public :: f_matrix
-  public :: f_matrix_tuple
   public :: f_matrix_memsize
   public :: f_matrix_worksize
   public :: f_matrix_eval
 !
+  integer(IK), parameter :: header_size = 2
+  integer(IK), parameter :: nn = 1
+  !! n * n
+  integer(IK), parameter :: nw = 2
+  !! work memory size.
+!
 !| f_matrix <br>
 !  - F is the n x n matrix. <br>
 !  - F(I,J) = min_{R, s} tr[R@C_IJS] <br>
-  type f_matrix
-    private
-    sequence
-    integer(IK)         :: nn
-    !! nn :: n * n
-    integer(IK)         :: nw
-    !! nw :: work memory size.
-  end type f_matrix
-!
-  interface f_matrix
-    module procedure f_matrix_new
-  end interface f_matrix
-!
-!| A set of f_matrix and work arrays. <br>
 !  This is mainly used for passing during initialization.
-  type f_matrix_tuple
-    type(f_matrix)        :: f
+  type f_matrix
+    integer(IK)           :: q(header_size)
     !! header
     real(RK), allocatable :: x(:)
     !! main memory.
     real(RK), allocatable :: w(:)
     !! work memory.
   contains
-    final :: f_matrix_tuple_destroy
-  end type f_matrix_tuple
+    final :: f_matrix_destroy
+  end type f_matrix
 !
-  interface f_matrix_tuple
-    module procedure f_matrix_tuple_new
-  end interface f_matrix_tuple
+  interface f_matrix
+    module procedure f_matrix_new
+  end interface f_matrix
 !
 contains
 !
 !| Constructer
   pure function f_matrix_new(b) result(res)
-    !| mol_block, must be initialized.
     integer(IK), intent(in) :: b(*)
-    type(f_matrix)          :: res
+    !! mol_block, must be initialized.
+    type(f_matrix)    :: res
 !
-    res%nn = mol_block_nmol(b)**2
-    res%nw = sdmin_worksize()
+    res%q(nn) = mol_block_nmol(b)**2
+    res%q(nw) = sdmin_worksize()
+
+    allocate (res%x(f_matrix_memsize(res%q)))
+    allocate (res%w(f_matrix_worksize(res%q)))
 !
   end function f_matrix_new
 !
-!| Constructer
-  pure function f_matrix_tuple_new(b) result(res)
-    !| mol_block, must be initialized.
-    integer(IK), intent(in) :: b(*)
-    type(f_matrix_tuple)    :: res
-!
-    res%f = f_matrix(b)
-    allocate (res%x(f_matrix_memsize(res%f)))
-    allocate (res%w(f_matrix_worksize(res%f)))
-!
-  end function f_matrix_tuple_new
-!
 !| Inquire memsize of f_matrix.
-  pure elemental function f_matrix_memsize(this) result(res)
-    !| this :: f_matrix
-    type(f_matrix), intent(in) :: this
-    integer(IK)                :: res
-    res = this%nn
+  pure function f_matrix_memsize(q) result(res)
+    integer(IK), intent(in) :: q(*)
+    !! f_matrix
+    integer(IK)             :: res
+    res = q(nn)
   end function f_matrix_memsize
 !
 !| Inquire worksize of f_matrix.
-  pure elemental function f_matrix_worksize(this) result(res)
-    !| this :: f_matrix
-    type(f_matrix), intent(in) :: this
-    integer(IK)                :: res
-    res = this%nw
+  pure function f_matrix_worksize(q) result(res)
+    integer(IK), intent(in) :: q(*)
+    !! f_matrix
+    integer(IK)             :: res
+    res = q(nw)
   end function f_matrix_worksize
 !
 !| Evaluation the D matrix.<br>
 !  If nx>=ny D(nx,ny), else D(ny,nx)
-  pure subroutine f_matrix_eval(this, cm, C, F, W)
-    type(f_matrix), intent(in)  :: this
-    !! f_matrix
-    type(c_matrix), intent(in)  :: cm
+  pure subroutine f_matrix_eval(q, qc, C, F, W)
+    integer(IK), intent(in) :: q(*)
+    !! header of f_matrix
+    integer(IK), intent(in) :: qc(*)
     !! header of covariacne matrix C.
-    real(RK), intent(inout)     :: C(*)
+    real(RK), intent(inout) :: C(*)
     !! covariacne matrix c.
-    real(RK), intent(inout)     :: F(*)
+    real(RK), intent(inout) :: F(*)
     !! main memory of F.
-    real(RK), intent(inout)     :: W(*)
+    real(RK), intent(inout) :: W(*)
     !! work array.
-    integer(IK)                 :: cb
+    integer(IK)             :: cb
 !
-    cb = c_matrix_blocksize(cm)
-    call eval_f_matrix(cb, this%nn, C, F, W)
+    cb = c_matrix_blocksize(qc)
+    call eval_f_matrix(cb, q(nn), C, F, W)
 !
   end subroutine f_matrix_eval
 !
@@ -126,11 +108,11 @@ contains
 !
   end subroutine eval_f_matrix
 !
-  pure elemental subroutine f_matrix_tuple_destroy(this)
-    type(f_matrix_tuple), intent(inout) :: this
+  pure elemental subroutine f_matrix_destroy(this)
+    type(f_matrix), intent(inout) :: this
     if (ALLOCATED(this%x)) deallocate (this%x)
     if (ALLOCATED(this%w)) deallocate (this%w)
-  end subroutine f_matrix_tuple_destroy
+  end subroutine f_matrix_destroy
 !
 end module mod_f_matrix
 
--- src/symRMSD/test/test_f_matrix.f90
+++ src/symRMSD/test/test_f_matrix.f90
@@ -33,7 +33,7 @@ contains
     integer(IK)           :: w1, w2, w3
 !
     X = sample(3, SIZE(X, 2))
-    Y = (ONe - s) * X + s * sample(3, SIZE(Y, 2))
+    Y = (ONE - s) * X + s * sample(3, SIZE(Y, 2))
 !
     b(1) = mol_block(5, 3)
     b(2) = mol_block(3, 2, sym=RESHAPE([2, 3, 1, 3, 1, 2], [3, 2]))
@@ -46,42 +46,41 @@ contains
     f(2) = f_matrix(b(2)%q)
     f(3) = f_matrix(b(3)%q)
 !
-    print*,c_matrix_memsize(c)
-    print*,c_matrix_worksize(c)
-    print*,f_matrix_memsize(f)
-    print*,f_matrix_worksize(f)
+    print *, c_matrix_memsize(c(1)%q), c_matrix_memsize(c(2)%q), c_matrix_memsize(c(3)%q)
+    print *, c_matrix_worksize(c(1)%q), c_matrix_worksize(c(2)%q), c_matrix_worksize(c(3)%q)
+    print *, f_matrix_memsize(f(1)%q), f_matrix_memsize(f(2)%q), f_matrix_memsize(f(3)%q)
+    print *, f_matrix_worksize(f(1)%q), f_matrix_worksize(f(2)%q), f_matrix_worksize(f(3)%q)
 !
     x1 = 1
     x2 = x1 + mol_block_natm(b(1)%q)
     x3 = x2 + mol_block_natm(b(2)%q)
 !
     c1 = 1
-    v1 = c1 + c_matrix_memsize(c(1))
+    v1 = c1 + c_matrix_memsize(c(1)%q)
     f1 = v1
-    w1 = f1 + f_matrix_memsize(f(1))
+    w1 = f1 + f_matrix_memsize(f(1)%q)
     c2 = w1
-    v2 = c2 + c_matrix_memsize(c(2))
+    v2 = c2 + c_matrix_memsize(c(2)%q)
     f2 = v2
-    w2 = f2 + f_matrix_memsize(f(2))
+    w2 = f2 + f_matrix_memsize(f(2)%q)
     c3 = w2
-    v3 = c3 + c_matrix_memsize(c(3))
+    v3 = c3 + c_matrix_memsize(c(3)%q)
     f3 = v3
-    w3 = f3 + f_matrix_memsize(f(3))
+    w3 = f3 + f_matrix_memsize(f(3)%q)
 !
-    allocate (W(SUM(c_matrix_memsize(c) + c_matrix_worksize(c) &
-   &              + f_matrix_memsize(f) + f_matrix_worksize(f))))
-!
+    W = [c(1)%x, c(2)%x, c(3)%x, f(1)%x, f(2)%x, f(3)%x, c(3)%w, f(3)%w]
     W(:) = 999
-    call c_matrix_eval(c(1), b(1)%q, X(1, x1), Y(1, x1), W(c1), W(v1))
-    call f_matrix_eval(f(1), c(1), W(c1), W(f1), W(w1))
-    call c_matrix_eval(c(2), b(2)%q, X(1, x2), Y(1, x2), W(c2), W(v2))
-    call f_matrix_eval(f(2), c(2), W(c2), W(f2), W(w2))
-    call c_matrix_eval(c(3), b(3)%q, X(1, x3), Y(1, x3), W(c3), W(v3))
-    call f_matrix_eval(f(3), c(3), W(c3), W(f3), W(w3))
 !
-    print'(3f9.3)', W(f1:f1 + f_matrix_memsize(f(1)) - 1)
-    print'(2f9.3)', W(f2:f2 + f_matrix_memsize(f(2)) - 1)
-    print'(3f9.3)', W(f3:f3 + f_matrix_memsize(f(3)) - 1)
+    call c_matrix_eval(c(1)%q, b(1)%q, X(1, x1), Y(1, x1), W(c1), W(v1))
+    call f_matrix_eval(f(1)%q, c(1)%q, W(c1), W(f1), W(w1))
+    call c_matrix_eval(c(2)%q, b(2)%q, X(1, x2), Y(1, x2), W(c2), W(v2))
+    call f_matrix_eval(f(2)%q, c(2)%q, W(c2), W(f2), W(w2))
+    call c_matrix_eval(c(3)%q, b(3)%q, X(1, x3), Y(1, x3), W(c3), W(v3))
+    call f_matrix_eval(f(3)%q, c(3)%q, W(c3), W(f3), W(w3))
+!
+    print'(3f9.3)', W(f1:f1 + f_matrix_memsize(f(1)%q) - 1)
+    print'(2f9.3)', W(f2:f2 + f_matrix_memsize(f(2)%q) - 1)
+    print'(3f9.3)', W(f3:f3 + f_matrix_memsize(f(3)%q) - 1)
     print*
 !
   end subroutine test0
