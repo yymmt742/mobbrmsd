60e2e491217736df1cfd76ee01bd46ae1e0827bb
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-03-27 17:18:08 +0900

    [update] python_driver.f90

--- src/python_interface/python_driver.f90
+++ src/python_interface/python_driver.f90
@@ -1,27 +1,13 @@
 module driver
   !$ use omp_lib
   use mod_params, only: IK, RK, ONE => RONE, ZERO => RZERO
-  use mod_symRMSD
-  use mod_branch_and_bound, only: DEF_maxeval, DEF_cutoff
-  use mod_mol_block
-  use mod_mol_symmetry
+  use mod_mobbrmsd
   implicit none
   private
-  public maxeval
-  public swap_y
   public add_molecule
-  public setup
   public run
-  public clear
 !
-  integer(kind=ik), save     :: maxeval = DEF_maxeval
-  real(kind=rk), save        :: cutoff  = DEF_cutoff
-  integer(kind=ik), save     :: dmn = 0
-  integer(kind=ik), save     :: nmem = 0
-  integer(kind=ik), save     :: njob = 0
-  logical, save              :: swap_y = .TRUE.
-  type(symRMSD_input), save  :: inp
-  type(symRMSD), allocatable :: sym(:)
+  type(mobbrmsd_input), save :: inp
 !
 contains
 !
@@ -30,71 +16,63 @@ contains
     integer(kind=ik), intent(in) :: n
     integer(kind=ik), intent(in) :: s
     integer(kind=ik), intent(in) :: sym(*)
-    type(mol_block)         :: b_
-    b_ = mol_block(1, s, m, n, m, n)
-    call inp%add_molecule(b_, sym)
-    dmn = inp%blk%mn * inp%blk%d
+!
+    call inp%add_molecule(m, n, sym=RESHAPE(sym(:m * (s - 1)), [m, s - 1]))
+!
   end subroutine add_molecule
 !
-  subroutine setup()
-    integer(kind=ik) :: i
-    inp%maxeval = maxeval
-    inp%cutoff  = cutoff
-    if (ALLOCATED(sym)) then
-      deallocate (sym)
-    end if
+  !| run mobbrmsd
+  subroutine run(x, y, cutoff, difflim, maxeval, n, rmsd, upper, lower, log_ratio, neval)
+    real(kind=rk), intent(in)     :: x(*)
+    !! reference coordinate
+    real(kind=rk), intent(inout)  :: y(*)
+    !! target coordinate
+    integer(kind=ik), intent(in)  :: maxeval
+    real(kind=rk), intent(in)     :: cutoff
+    real(kind=rk), intent(in)     :: difflim
+    integer(kind=ik), intent(in)  :: n
+    real(kind=rk), intent(out)    :: rmsd(n)
+    real(kind=rk), intent(out)    :: upper(n)
+    real(kind=rk), intent(out)    :: lower(n)
+    real(kind=rk), intent(out)    :: log_ratio(n)
+    integer(kind=ik), intent(out) :: neval(n)
+    real(kind=rk), allocatable    :: W(:, :)
+    integer(kind=ik)              :: nmem
+    integer(kind=ik)              :: njob
+    type(mobbrmsd)                :: mob
+    integer(kind=ik)              :: i, dmn
+!
+    mob = mobbrmsd(inp)
+    nmem = mob%h%memsize()
+    dmn = mob%h%n_atoms()
+!
     !$omp parallel
     njob = MAX(omp_get_num_threads(), 1)
     !$omp end parallel
-    ALLOCATE(sym(njob))
-    do concurrent(i=1:njob)
-      sym(i) = symRMSD(inp)
-    end do
-    nmem = sym(1)%nmem
-  end subroutine setup
 !
-  subroutine run(x, y, n, rmsd, log_ratio, nsearch, rmsd_with_error)
-    real(kind=rk), intent(in)     :: x(*)
-    real(kind=rk), intent(inout)  :: y(*)
-    integer(kind=ik), intent(in)  :: n
-    real(kind=rk), intent(out)    :: rmsd(n)
-    real(kind=rk), intent(out)    :: log_ratio(n)
-    integer(kind=ik), intent(out) :: nsearch(n)
-    real(kind=rk), intent(out)    :: rmsd_with_error(2, n)
-    real(kind=rk)                 :: w(nmem, njob)
-    integer(kind=ik)              :: i
+    allocate (W(nmem, njob))
 !
     !$omp parallel do
     do i = 1, n
       block
-        integer(kind=ik) :: ijob, pnt
-        real(kind=rk)    :: rat(3)
+        integer(kind=ik)     :: ijob, pnt
+        type(mobbrmsd_state) :: s
+        real(kind=rk)        :: rat(3)
 !
         pnt = (i - 1) * dmn + 1
         ijob = omp_get_thread_num() + 1
-        call sym(ijob)%run(swap_y, x, y(pnt), w(1, ijob))
+        s = mob%s
+        call mobrmsd_run(mob%h, s, X, Y(pnt), w(1, ijob), cutoff, difflim, maxeval)
 !
-        rmsd(i) = sym(ijob)%rmsd(w(1, ijob))
-        rat = sym(ijob)%search_ratio(w(1, ijob))
-        log_ratio(i) = rat(1)
-        nsearch(i) = NINT(rat(3))
-        rmsd_with_error(:,i) = sym(ijob)%rmsd_with_error(w(1, ijob))
+        rmsd(i) = s%rmsd()
+        upper(i) = s%upperbound()
+        lower(i) = s%lowerbound()
+        log_ratio(i) = s%log_eval_ratio()
 !
       end block
     end do
     !$omp end parallel do
 
   end subroutine run
-!
-  subroutine clear()
-    call inp%clear()
-    if (ALLOCATED(sym)) then
-      call sym%clear()
-      deallocate (sym)
-    end if
-    nmem = 0
-    njob = 0
-    dmn = 0
-  end subroutine clear
 !
 end module driver
