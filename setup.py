a09f0230178475124226244f6781ab3ed5edf493
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-04-10 16:25:24 +0900

    [update] setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from skbuild import setup
 
 setup(
-  name="mobbrmsd",
-  version="0.0.1",
-  description="molecular orientation corrected rmsd with branch-and-bound",
-  packages=['mobbrmsd'],
-  python_requires=">=3.8",
- )
+    name='mobbrmsd',
+    version="0.0.1",
+    author="yymmt742",
+    author_email="yymmt@kuchem.kyoto-u.ac.jp",
+    packages=["mobbrmsd"],
+    cmake_install_dir="mobbrmsd/bin",
+    url="https://github.com/yymmt742/mobbrmsd",
+    download_url="https://github.com/yymmt742/mobbrmsd",
+    description="molecular orientation corrected rmsd with branch-and-bound",
+    long_description="molecular orientation corrected rmsd with branch-and-bound",
+    classifiers=[
+                'Programming Language :: Fortran',
+                'Programming Language :: Python'
+                ],
+    license="MIT",
+    python_requires=">=3.8"
+    )
