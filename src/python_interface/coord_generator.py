05757f106bbc946e9aa07166a58b9939c1e1f35b
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-04-12 18:29:25 +0900

    [add] coord_generator.py

--- src/python_interface/coord_generator.py
+++ src/python_interface/coord_generator.py
@@ -1,67 +1,137 @@
-import numpy as np
+from typing import Any, Iterable
+import numpy
+import matplotlib.pyplot as plt
 
-d = 3
-rng = np.random.default_rng()
 
-def so2():
-    g = rng.random(1)
-    a = np.cos(2*np.pi*g[0])
-    b = np.sin(2*np.pi*g[0])
-    return np.array([ ( a,-b ),
-                      ( b, a ) ])
+class so_generator:
+    def __init__(self, d: int = 3):
 
-def so3():
-    g = rng.random(4)
-    a = g[1:] / np.linalg.norm(g[1:])
-    b = np.sin(2*np.pi*g[0])
-    c = np.cos(2*np.pi*g[0])
-    s = 1.0 - c
-    return np.array([
-        [ a[0] * a[0] * s + c,        a[0] * a[1] * s - a[2] * b, a[0] * a[2] * s + a[1] * b ],
-        [ a[0] * a[1] * s + a[2] * b, a[1] * a[1] * s + c,        a[1] * a[2] * s - a[0] * b ],
-        [ a[0] * a[2] * s - a[1] * b, a[1] * a[2] * s + a[0] * b, a[2] * a[2] * s + c        ] ])
+        self.rng = numpy.random.default_rng()
 
-def mol(m, n, s):
-    X0 = rng.standard_normal((m, d))
-    X1 = rng.standard_normal((n, m, d))
-    return X1
+        def so_1():
+            return numpy.array([[1.0]])
 
-def gen(m, n, a, b):
+        def so_2():
+            g = 2 * numpy.pi * self.rng.random(1)
+            a = numpy.cos(g[0])
+            b = numpy.sin(g[0])
+            return numpy.array([[a, -b], [b, a]])
 
-    a0 = np.sin(0.5*np.pi*a)
-    a1 = np.cos(0.5*np.pi*a)
-    b0 = np.sin(0.5*np.pi*b)
-    b1 = np.cos(0.5*np.pi*b)
+        def so_3():
+            q = self.rng.random(4)
+            q /= numpy.linalg.norm(q)
+            qq = numpy.power(q, 2)
+            q02 = 2 * q[0] * q[2]
+            q03 = 2 * q[0] * q[3]
+            q12 = 2 * q[1] * q[2]
+            q13 = 2 * q[1] * q[3]
+            q01 = 2 * q[0] * q[1]
+            q23 = 2 * q[2] * q[3]
 
-    x0 = rng.standard_normal((m, d))
-    x1 = rng.standard_normal((n, m, d))
-    cx = rng.standard_normal((n, d))
+            return numpy.array(
+                [
+                    [
+                        qq[0] + qq[1] - qq[2] - qq[3],
+                        q12 - q03,
+                        q13 + q02,
+                    ],
+                    [
+                        q12 + q03,
+                        qq[0] - qq[1] + qq[2] - qq[3],
+                        q23 - q01,
+                    ],
+                    [
+                        q13 - q02,
+                        q23 + q01,
+                        qq[0] - qq[1] - qq[2] + qq[3],
+                    ],
+                ]
+            )
 
-    for i in range(n):
-        x1[i] = b0 * cx[i].reshape([1, d]) + b1 * (a0 * x1[i] + a1 * x0)@so3()
-    x1 -= np.mean(x1.reshape((n * m, d)), 0).reshape([1, d])
+        def so_g():
+            g = self.rng.random([d, d]) @ self.rng.random([d, d])
+            u, s, v = numpy.linalg.svd(g)
+            det = numpy.linalg.det(u @ v)
+            if det < 0.0:
+                u[-1] = -u[-1]
+            return u @ v
 
-    return x1.reshape([n * m, d])
+        if d < 2:
+            self.generate = so_1
+        elif d == 2:
+            self.generate = so_2
+        elif d == 3:
+            self.generate = so_3
+        else:
+            self.generate = so_g
 
-if (__name__=='__main__'):
-  m = 5
-  n = 5
-  z = 1.5 / np.sqrt(0.1)
-  x0 = rng.standard_normal((m, 2))
-  x1 = rng.standard_normal((n, m, 2))
-  cx = rng.standard_normal((n, 2))
-  x2 = np.zeros((n, m, 2))
-  so = np.array([so2() for i in range(n)])
 
-  for s in (0.2, 1.0):
-    b0 = np.cos(0.5*np.pi*s)
-    b1 = np.sin(0.5*np.pi*s)
-    for r in (0.0, 1.0):
-      a0 = np.sin(0.5*np.pi*r)
-      a1 = np.cos(0.5*np.pi*r)
-      for i in range(n):
-          x2[i] = b0 * cx[i].reshape([1, 2]) + b1 * (a0 * x1[i] + a1 * x0)@so[i]
-      sa = '{:d}'.format(round(r*100)).zfill(3)
-      sb = '{:d}'.format(round(s*100)).zfill(3)
-      path = 'sample_'+sa+'_'+sb+'.npy'
-      np.save(path, x2)
+class coord_generator:
+    def __init__(self, d: int = 3):
+        self.d = d
+        self.sog = so_generator(d)
+        self.rng = numpy.random.default_rng()
+
+    def generate(
+        self, n: int, m: int, a: float | Iterable[float], b: float | Iterable[float]
+    ) -> numpy.ndarray:
+
+        Xstr = self.rng.standard_normal((m, n, self.d))
+        temp = self.rng.standard_normal((n, self.d))
+        Xtem = numpy.array([temp @ self.sog.generate() for i in range(m)])
+        Xvar = self.rng.standard_normal((m, 1, self.d))
+
+        def x_sample(a, b, Xvar, Xtem, Xstr):
+            a_ = 0.5 * numpy.pi * a
+            b_ = 0.5 * numpy.pi * b
+
+            sa = numpy.sin(a_)
+            ca = numpy.cos(a_)
+            sb = numpy.sin(b_)
+            cb = numpy.cos(b_)
+
+            return ca * (cb * Xstr + sb * Xtem) + sa * Xvar
+
+        if isinstance(a, float):
+            if isinstance(b, float):
+                return x_sample(a, b, Xvar, Xtem, Xstr)
+            elif isinstance(b, Iterable):
+                return numpy.array([x_sample(a, bi, Xvar, Xtem, Xstr) for bi in b])
+        elif isinstance(a, Iterable):
+            if isinstance(b, float):
+                return numpy.array([x_sample(ai, b, Xvar, Xtem, Xstr) for ai in a])
+            elif isinstance(b, Iterable):
+                return numpy.array(
+                    [[x_sample(ai, bi, Xvar, Xtem, Xstr) for bi in b] for ai in a]
+                )
+
+
+if __name__ == "__main__":
+    import itertools
+
+    cogen = coord_generator(2)
+    n = 3
+    m = 5
+    a = (0.0, 0.8, 0.9, 0.99)
+    b = (0.0, 1.0)
+    x = cogen.generate(n, m, a, b)
+
+    print("--- coord_generator.py ---")
+    print("This is sampled coordinates.\n")
+    for i, j in itertools.product((range(len(a))), (range(len(b)))):
+        print("    a = ", a[i], "b = ", b[j], "\n")
+        print(x[i, j], "\n")
+
+"""
+        sa = "{:d}".format(round(a[i] * 100)).zfill(3)
+        sb = "{:d}".format(round(b[j] * 100)).zfill(3)
+        path = "sample_" + sa + "_" + sb
+        plt.xlim(-3.0, 3.0)
+        plt.ylim(-3.0, 3.0)
+        for xk in x[i, j]:
+            plt.plot(xk[:, 0], xk[:, 1])
+            plt.scatter(xk[:, 0], xk[:, 1])
+        plt.savefig(path + ".eps")
+        plt.savefig(path + ".png")
+        plt.clf()
+"""
