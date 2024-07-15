53eb81a1987a0b18ef18df88ebae37aff1967e69
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-07-15 22:30:38 +0900

    [wip]

--- src/mobbrmsd/test/spline.py
+++ src/mobbrmsd/test/spline.py
@@ -3,45 +3,228 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 
 
-def spline_forward(f0, f1, f2, x0, x1, a0):
-    dx = x1 - x0
-    A = (f0(x1) - f0(x0)) / (dx * dx) - f1(x0) / dx
-    B = (f1(x1) - f1(x0)) / dx
-    y0 = f0(x0)
-    y1 = f1(x0)
-    a1 = 12 * A - 6 * B - a0
-    # a1 = -12 * A + 6 * B + a0
-    b0 = (24 * A - 6 * B - 6 * a0) / dx
-    c0 = (-72 * A + 24 * B + 12 * a0) / (dx * dx)
-    print(f"y0 {a0:9.3f} {b0:9.3f} {c0:9.3f} {a1:9.3f} ")
+def teylar_0(x):
     return (
-        a1,
-        lambda x: (
-            y0
-            + y1 * (x - x0)
-            + (a0 / 2) * np.power(x - x0, 2)
-            + (b0 / 6) * np.power(x - x0, 3)
-            + (c0 / 24) * np.power(x - x0, 4)
-        ),
+        1 / 6 * (-3 + 3 * np.sqrt(3) - np.sqrt(6))
+        + 1 / 12 * (2 - np.sqrt(6)) * x
+        + ((3 * np.sqrt(2) - 4) * np.power(x, 2)) / (48 * np.sqrt(3))
+        + (2 / 81 - 1 / (16 * np.sqrt(6))) * np.power(x, 3)
+        + (5 * (81 * np.sqrt(2) - 112) * np.power(x, 4)) / (20736 * np.sqrt(3))
     )
 
 
-f0 = lambda x: np.cos(np.arccos(x) / 3.0) - (np.sqrt(1 + x)) / 2 - 0.5
-f1 = lambda x: (
-    (np.sin(np.arccos(x) / 3) / (3 * np.sqrt(1.0 - x)) - 0.25) / np.sqrt(x + 1.0)
-)
-f2 = lambda x: (
-    -np.cos(np.arccos(x) / 3) / (9 * (1 - np.power(x, 2)))
-    + (x * np.sin(np.arccos(x) / 3) / (3 * np.power(np.sqrt(1 - x), 3)) + 0.125)
-    / np.power(np.sqrt(1 + x), 3)
-)
-x = np.array([-1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0])
-y = f0(x)
-# S = interpolate.interp1d(x, y, kind="cubic")
-c = interpolate.PPoly.from_spline(interpolate.splrep(x, y)).c.T
-print(c)
+def spline_quat(x, f0, f1, f2):
+    y = f0(x)
+    y_ = f1(x)
+    dx = x[1:] - x[:-1]
+    dy = y[1:] - y[:-1] - dx * y_[:-1]
+    m = np.zeros((3 * dx.size, 3 * dx.size))
+    p = np.zeros((3 * dy.size))
+    j = 0
+    for dxi, dyi in zip(dx, dy):
+        p[j] = dyi
+        m[j, j] = dxi * dxi / 2
+        m[j, j + 1] = dxi * dxi * dxi / 6
+        m[j, j + 2] = dxi * dxi * dxi * dxi / 24
+        if j + 3 < m.shape[1]:
+            m[j + 1, j] = dxi
+            m[j + 1, j + 1] = dxi * dxi / 2
+            m[j + 1, j + 2] = dxi * dxi * dxi / 6
+            m[j + 2, j] = 1.0
+            m[j + 2, j + 1] = dxi
+            m[j + 2, j + 2] = dxi * dxi / 2
+            m[j + 2, j + 3] = -1.0
+        else:
+            m[j + 1, 2] = 1.0
+            m[j + 2, j + 2] = 1.0
 
-t = np.arange(-0.99, 0.99, 0.01)
+        j += 3
+
+    w = np.linalg.solve(m, p).reshape([-1, 3])
+
+    def s(t):
+        if t < x[0]:
+            return 0.0
+        for i in range(w.size):
+            if t < x[i + 1]:
+                return (
+                    y[i]
+                    + y_[i] * (t - x[i])
+                    + (w[i, 0] / 2) * np.power(t - x[i], 2)
+                    + (w[i, 1] / 6) * np.power(t - x[i], 3)
+                    + (w[i, 2] / 24) * np.power(t - x[i], 4)
+                )
+
+        return y[-1]
+
+    return np.vectorize(s)
+
+
+def spline_cubic(x, f0, f1):
+    y = f0(x)
+    dx = x[1:] - x[:-1]
+    dy = y[1:] - y[:-1]
+    m = np.zeros((3 * dx.size, 3 * dx.size))
+    l = np.zeros((3 * dx.size, 3 * dx.size))
+    p = np.zeros((3 * dy.size))
+    j = 0
+    for dxi, dyi in zip(dx, dy):
+        p[j] = dyi
+        m[j, j] = dxi
+        m[j, j + 1] = dxi * dxi / 2
+        m[j, j + 2] = dxi * dxi * dxi / 6
+        if j + 3 < m.shape[1]:
+            m[j + 1, j] = 1.0
+            m[j + 1, j + 1] = dxi
+            m[j + 1, j + 2] = dxi * dxi / 2
+            m[j + 2, j + 1] = 1.0
+            m[j + 2, j + 2] = dxi
+            m[j + 1, j + 3] = -1.0
+            m[j + 2, j + 4] = -1.0
+        else:
+            m[j + 1, 2] = 1.0
+            m[j + 2, j + 2] = 1.0
+        l[j, j] = dxi
+        l[j, j + 1] = -dxi * dxi / 2
+        l[j, j + 2] = dxi * dxi * dxi / 6
+        if j > 0:
+            l[j + 1, j] = 1.0
+            l[j + 1, j + 1] = -dxi
+            l[j + 1, j + 2] = dxi * dxi / 2
+            l[j + 2, j + 1] = 1.0
+            l[j + 2, j + 2] = -dxi
+            l[j + 1, j - 3] = -1.0
+            l[j + 2, j - 2] = -1.0
+        else:
+            l[1, -1] = 1.0
+            l[2, 2] = 1.0
+        j += 3
+
+    w = np.linalg.solve(m, p).reshape([-1, 3])
+    v = np.linalg.solve(l, p).reshape([-1, 3])
+
+    def s0(t):
+        if t < x[0]:
+            return 0.0
+        for i in range(w.shape[0]):
+            if t < x[i + 1]:
+                return (
+                    y[i + 1]
+                    + v[i, 0] * (t - x[i + 1])
+                    + (v[i, 1] / 2) * np.power(t - x[i + 1], 2)
+                    + (v[i, 2] / 6) * np.power(t - x[i + 1], 3)
+                )
+
+        return (
+            y[-1]
+            + v[-1, 0] * (t - x[-1])
+            + (v[-1, 1] / 2) * np.power(t - x[-1], 2)
+            + (v[-1, 2] / 6) * np.power(t - x[-1], 3)
+        )
+
+    def s1(t):
+        for i in range(w.shape[0]):
+            if t < x[i + 1]:
+                return (
+                    y[i]
+                    + w[i, 0] * (t - x[i])
+                    + (w[i, 1] / 2) * np.power(t - x[i], 2)
+                    + (w[i, 2] / 6) * np.power(t - x[i], 3)
+                )
+
+        return (
+            y[-1]
+            + w[-1, 0] * (t - x[-1])
+            + (w[-1, 1] / 2) * np.power(t - x[-1], 2)
+            + (w[-1, 2] / 6) * np.power(t - x[-1], 3)
+        )
+
+    return np.vectorize(lambda t: (s0(t) + s1(t)) / 2)
+
+
+f0 = np.vectorize(
+    lambda x: (
+        np.cos(np.arccos(x) / 3.0)
+        - (np.sqrt(1 + x)) / np.sqrt(6)
+        - 0.5
+        + (x + 1) / 18
+        - (5 * np.power(x + 1, 3 / 2)) / (108 * np.sqrt(6))
+        + 2 / 243 * np.power(x + 1, 2)
+        - (77 * np.power(x + 1, 5 / 2)) / (7776 * np.sqrt(6))
+        + 14 * np.power(x + 1, 3) / 6561
+        if np.abs(x) < 1.0
+        else (
+            (
+                np.cosh(np.log(x + np.sqrt(np.power(x, 2) - 1)) / 3)
+                - (np.sqrt(1 + x)) / np.sqrt(6)
+                - 0.5
+                + (x + 1) / 18
+                - (5 * np.power(x + 1, 3 / 2)) / (108 * np.sqrt(6))
+                + 2 / 243 * np.power(x + 1, 2)
+                - (77 * np.power(x + 1, 5 / 2)) / (7776 * np.sqrt(6))
+                + 14 * np.power(x + 1, 3) / 6561
+            )
+            if x > 0.0
+            else 0.0
+        )
+    )
+)
+f1 = np.vectorize(
+    lambda x: (
+        (np.sin(np.arccos(x) / 3) / (3 * np.sqrt(1 - x)) - 1 / (2 * np.sqrt(6)))
+        / np.sqrt(x + 1)
+        + 14 * np.power(x + 1, 2) / 2187
+        - 385 * np.power(x + 1, 5 / 2) / (15552 * np.sqrt(6))
+        + 4 * (x + 1) / 243
+        - 5 * np.sqrt(x + 1) / (72 + np.sqrt(6))
+        + 1 / 18
+        if np.abs(x) < 1.0
+        else (
+            (
+                np.sinh(np.log(x + np.sqrt(np.power(x, 2) - 1)) / 3)
+                / (3 * np.sqrt(np.power(x, 2) - 1))
+                + 14 * 4 / 2187
+                - 385 * 2 / (15552 * np.sqrt(3))
+                + 8 / 243
+                - 5 / (72 * np.sqrt(3))
+                - 1 / (4 * np.sqrt(3))
+                + 1 / 18
+            )
+            if x > 0.0
+            else 0.0
+        )
+    )
+)
+x = np.arange(-2.0, 2.0, 0.1)
+# x = np.array([-1.0, -0.75, -0.5, -0.25, 0.0])
+# s = spline_quat(x, f0, f1, f2)
+s = spline_cubic(x, f0, f1)
+t = np.arange(-1.5, 1.5, 0.001)
+"""
+for xi, yi, wi in zip(x, y, w):
+    s = (
+        lambda t: yi
+        + wi[0] * (t - xi)
+        + (wi[1] / 2) * np.power(t - xi, 2)
+        + (wi[2] / 6) * np.power(t - xi, 3)
+    )
+    plt.plot(t, s(t), label="s" + str(xi))
+"""
+
+
+# plt.plot(t, teylar_0(t) - f0(t), label="teylar")
+plt.plot(t, s(t) - f0(t), label="S")
+# plt.plot(t, s(t), label="S")
+# plt.plot(t, f0(t), label="f", ls=":")
+# plt.ylim([-1.1, 1.1])
+# plt.ylim([-1.0e-6, 1.0e-6])
+plt.legend()
+plt.show()
+exit()
+
+# S = interpolate.interp1d(x, y, kind="cubic")
+# c = interpolate.PPoly.from_spline(interpolate.splrep(x, y)).c.T
+# print(c)
 for ci, xi in zip(c, x):
     s = (
         lambda x: ci[3]
