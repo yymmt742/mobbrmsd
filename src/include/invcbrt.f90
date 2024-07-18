217ed0aecc34637fdf954292fadb407f9c019550
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-07-18 12:26:12 +0900

    [add] test_invcbrt.f90

--- src/include/invcbrt.f90
+++ src/include/invcbrt.f90
@@ -1,36 +1,20 @@
-subroutine invcbrt(x)
+pure elemental function invcbrt(x) result(res)
+  use mod_kinds, only: I4, R4, I8
   implicit none
-  real    :: x, fx, c, y
-  integer :: iz
-  x = 6.3 * 10
-  fx = x / 3
-  iz = INT(z"54a21d2a") - TRANSFER(x, iz) / 3
-  !iz = INT(z"548c39cb") - TRANSFER(x, iz) / 3
-  !iz = INT(z"54a21d2a") - TRANSFER(x, iz) / 3
-  !print'(b64)', TRANSFER(ISHFT(TRANSFER(x, iz) * INT(z"55555556", INT64), -32), 0_INT32)
-  !print'(b64)', INT(z"54A21D2A", INT32)
-  !iz = INT(z"54A21D2A") - TRANSFER(x, iz) / 3
-  !iz = INT(z"54A21D2A", INT32) - TRANSFER(ISHFT(TRANSFER(x, iz) * INT(z"55555556", INT64), -32), 0_INT32)
-  print'(b64)', iz
-  y = TRANSFER(iz, y)
-  print'(b64.64)', iz
-  print'(b64.64)', TRANSFER(y, iz)
-  print *, 0, y, y - 1 / (x**(1 / 3.0))
-  c = y * y * y * x
-  !y = y * (1.5555555555 - c * (0.7777777777 - 0.2222222222 * c))
-  y = y * (4.0 / 3 - y * y * y * fx)
-  print *, 1, y, y**3, 1 / y**3, y - 1 / (x**(1 / 3.0))
-  c = 1.0 - y * y * y * x
-  y = y * (4.0 / 3 - y * y * y * fx)
-  !y = y * (1.0000000000 - 0.3333333333 * c)
-  print *, 2, y, y**3, 1 / y**3, y - 1 / (x**(1 / 3.0))
-  !y = y * (1.5015480449 - 0.534850249 * y * y * y * x)
-  !print *, y, y - 1 / (x**(1 / 3.0))
-  !y = y * (1.333333985 - 0.33333333 * y * y * y * x)
-  y = y * (4.0 / 3 - y * y * y * fx)
-  print *, 3, y, y**3, 1 / y**3, y - 1 / (x**(1 / 3.0))
-  y = y * (4.0 / 3 - y * y * y * fx)
-  print *, 4, y, y**3, 1 / y**3, y - 1 / (x**(1 / 3.0))
-  y = y * (4.0 / 3 - y * y * y * fx)
-  print *, 5, y, y**3, 1 / y**3, y - 1 / (x**(1 / 3.0))
-  end subroutine invcbrt(x)
+  real(RK), intent(in) :: x
+  real(RK)             :: res, c
+#ifdef USE_REAL32
+  res = TRANSFER(INT(z"548C2B4B", I4) - TRANSFER(x, 0_I4) / 3, x)
+  !res = TRANSFER(INT(z"54A21D2A", I4) - TRANSFER(x, 0_I4) / 3, x)
+#else
+  real(R4)             :: y
+  y = x
+  res = TRANSFER(INT(z"548C2B4B", I4) - TRANSFER(y, 0_I4) / 3, y)
+#endif
+  c = res * res * res * x
+  res = res * (1.752319676_RK - c * (1.2509524245_RK - 0.5093818292_RK * c))
+  c = 1.0_RK - res * res * res * x
+  res = res * (1.0_RK + 0.333333333333333_RK * c)
+! res = res * ((4.0_RK / 3.0_RK) - res * res * res * fx)
+! res = res * ((4.0_RK / 3.0_RK) - res * res * res * fx)
+end function invcbrt
---
+++ src/mobbrmsd/test/test_invcbrt.f90
@@ -0,0 +1,92 @@
+program main
+  use mod_kinds
+  use mod_params, only: RK, IK
+  use mod_unittest
+  implicit none
+  type(unittest) :: u
+#ifdef USE_REAL32
+  integer, parameter    :: place = 4
+#else
+  integer, parameter    :: place = 8
+#endif
+!
+  call u%init('test invcbrt')
+  call test1()
+  call u%init('test invcbrt time')
+  call test2()
+  call u%finish_and_terminate()
+!
+contains
+!
+#include "invcbrt.f90"
+!
+  subroutine test1()
+    real(RK) :: x, y, z, e1, e2
+    integer  :: i
+    do i = -100, 100
+      x = EXP(i * 0.1)
+      y = invcbrt(x)
+      z = invcbrt(x * x)
+      e1 = (x - 1 / y**3) / x
+      e2 = (x - (x * z)**3) / x
+      call u%assert_is_zero(e1, 'x = f(x)**(-3)', place=place)
+      call u%assert_is_zero(e2, 'x = (x * f(x*x))**3', place=place)
+    end do
+  end subroutine test1
+!
+  subroutine test2()
+    integer(IK), parameter :: N = 5000000, M = 10
+    real(RK)               :: A(N), B(N), C(N)
+    real(RK)               :: time_begin_s, time_end_s, time1, time2, tot1, tot2
+    real(RK)               :: merr1, merr2, nerr1, nerr2
+    integer                :: i
+    tot1 = 0.0_RK
+    tot2 = 0.0_RK
+    merr1 = 0.0_RK
+    merr2 = 0.0_RK
+    nerr1 = 0.0_RK
+    nerr2 = 0.0_RK
+    print'(A)', "                   implement       intrinsic"
+    print'(A)', "            --------------------------------"
+    do i = 1, M
+      call RANDOM_NUMBER(A)
+      A = EXP((A - 0.5_RK) * 10.0_RK)
+!
+      call CPU_TIME(time_begin_s)
+      B = invcbrt(A)
+      call CPU_TIME(time_end_s)
+      time1 = 1000 * (time_end_s - time_begin_s)
+      tot1 = tot1 + time1
+      merr1 = merr1 + mean_error(N, A, B)
+      nerr1 = nerr1 + norm_error(N, A, B)
+!
+      call CPU_TIME(time_begin_s)
+      C = A**(-1.0_RK / 3.0_RK)
+      call CPU_TIME(time_end_s)
+      time2 = 1000 * (time_end_s - time_begin_s)
+      tot2 = tot2 + time2
+      merr2 = merr2 + mean_error(N, A, C)
+      nerr2 = nerr2 + norm_error(N, A, C)
+      print '(I12,2f16.3)', i, time1, time2
+    end do
+    print'(A)', "            --------------------------------"
+    print'(12X,2f16.3)', tot1, tot2
+    print'(A,2f16.9)', "  mean error", merr1 / M, merr2 / M
+    print'(A,2f16.9)', "  norm error", nerr1 / M, nerr2 / M
+  end subroutine test2
+!
+  pure function mean_error(n, X, Y) result(res)
+    integer, intent(in)  :: n
+    real(RK), intent(in) :: X(n), Y(n)
+    real(RK)             :: res
+    res = SQRT(SUM((X - 1.0_RK / Y**3)**2) / SIZE(X))
+  end function mean_error
+!
+  pure function norm_error(n, X, Y) result(res)
+    integer, intent(in)  :: n
+    real(RK), intent(in) :: X(n), Y(n)
+    real(RK)             :: res
+    res = MAXVAL(ABS(X - 1.0_RK / Y**3))
+  end function norm_error
+!
+end program main
