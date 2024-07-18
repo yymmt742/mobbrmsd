cbe3b923f545394df0ed5802a33f641e76d30ae0
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-07-18 15:33:56 +0900

    [bugfix] invcbrt.f90, negative case

--- src/include/invcbrt.f90
+++ src/include/invcbrt.f90
@@ -3,14 +3,9 @@ pure elemental function invcbrt(x) result(res)
   implicit none
   real(RK), intent(in) :: x
   real(RK)             :: res, c
-#ifdef USE_REAL32
-  res = TRANSFER(INT(z"548C2B4B", I4) - TRANSFER(x, 0_I4) / 3, x)
-  !res = TRANSFER(INT(z"54A21D2A", I4) - TRANSFER(x, 0_I4) / 3, x)
-#else
   real(R4)             :: y
-  y = x
-  res = TRANSFER(INT(z"548C2B4B", I4) - TRANSFER(y, 0_I4) / 3, y)
-#endif
+  y = ABS(x)
+  res = SIGN(1.0_RK, x) * TRANSFER(INT(z"548C2B4B", I4) - TRANSFER(y, 0_I4) / 3, y)
   c = res * res * res * x
   res = res * (1.752319676_RK - c * (1.2509524245_RK - 0.5093818292_RK * c))
   c = 1.0_RK - res * res * res * x
