f4a02c8b885deb507bada4c7ac7536d5b6e925b2
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-08-16 09:11:43 +0900

    [update] __main__.py

--- src/python_interface/__main__.py
+++ src/python_interface/__main__.py
@@ -11,84 +11,37 @@ import numpy
 import time
 import pick
 
-bar1 = (
-    "  ------------------------------------------------------------------------------"
-)
-bar2 = (
-    "  =============================================================================="
-)
-bar3 = "==========================================="
 
-demo_list = [
-    demo_bb._demo_bb(),
-    demo_bb_multi._demo_bb_multi(),
-    demo_cogen._demo_cogen(),
-    demo_batch._demo_batch(),
-    demo_batch_tri._demo_batch_tri(),
-    demo_mst._demo_mst(),
-]
-demo1 = demo_cogen.title
-demo2 = demo_bb.title
-demo3 = demo_bb_sp.title
-demo4 = demo_bb_multi.title
-demo5 = demo_batch.title
-demo6 = demo_batch_tri.title
-demo7 = demo_mst.title
-exit_ = "exit"
-# opts = (demo1, demo2, demo3, demo4, demo5, demo6, demo7, exit_)
-opts = [l.title for l in demo_list] + ["exit"]
+def main():
+    demo_list = [
+        demo_bb._demo_bb(),
+        demo_bb_multi._demo_bb_multi(),
+        demo_cogen._demo_cogen(),
+        demo_batch._demo_batch(),
+        demo_batch_tri._demo_batch_tri(),
+        demo_mst._demo_mst(),
+    ]
+    opts = [l.title for l in demo_list] + ["exit"]
 
+    ver = "--- demonstration of mobbrmsd ver." + __version__.__version__ + " ---"
+    bar = "=" * len(ver)
+    title = bar + "\n" + ver + "\n" + bar + "\n" + " - Select demo code :"
 
-def run_demo(demo, read_input, after=None):
-
-    print(bar2)
-
-    prms = read_input()
-
-    start_wallclock_time = time.time()
-    start_cpu_time = time.process_time()
-
-    ret = demo(**prms)
-
-    end_wallclock_time = time.time()
-    end_cpu_time = time.process_time()
-    elapsed_cpu_time = end_wallclock_time - start_wallclock_time
-    elapsed_wallclock_time = end_wallclock_time - start_wallclock_time
-
-    print("    -- Elapsed times --")
-    print(
-        f"       cpu time : {elapsed_cpu_time:16.3f} sec",
-        f" wallclock time : {elapsed_wallclock_time:16.3f} sec",
-    )
-    print(bar2, "\n")
-
-    if after is not None:
-        after(ret)
-        print(bar2, "\n")
-
-
-title = (
-    bar3
-    + "\n"
-    + "--- demonstration of mobbrmsd ver."
-    + __version__.__version__
-    + " ---\n"
-    + bar3
-    + "\n"
-    + "    Select demo code :"
-)
-
-while True:
-    option, index = pick.pick(opts, title, indicator="   >")
-    if option == "exit":
-        break
-    for l in demo_list:
-        if option == l.title:
-            l.run_demo()
+    while True:
+        option, index = pick.pick(opts, title, indicator="   >")
+        if option == "exit":
             break
-    inp = input('  press any key to continue ("q" to exit) >> ')
-    print(inp)
-    if inp == "":
-        continue
-    if inp[0] == "q":
-        break
+        for l in demo_list:
+            if option == l.title:
+                l.run_demo()
+                break
+        inp = input('  press any key to continue ("q" to exit) >> ')
+        print(inp)
+        if inp == "":
+            continue
+        if inp[0] == "q":
+            break
+
+
+if __name__ == "__main__":
+    main()
