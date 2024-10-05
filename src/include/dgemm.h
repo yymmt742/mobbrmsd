603cd25e38002ccbc8e7fb029f92c2defa47b610
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-10-05 13:40:10 +0900

    [clean]  remove some unused files

--- src/include/dgemm.h
+++
@@ -1,9 +0,0 @@
-pure subroutine DGEMM(TRANSA, TRANSB, M, N, K, ALPHA, A, LDA, B, LDB, BETA, C, LDC)
-  use, intrinsic :: ISO_FORTRAN_ENV, only: rk => REAL64
-  real(rk), intent(in)     :: ALPHA, BETA
-  integer, intent(in)      :: K, LDA, LDB, LDC, M, N
-  character(*), intent(in) :: TRANSA, TRANSB
-  real(rk), intent(in)     :: A(LDA, *), B(LDB, *)
-  real(rk), intent(inout)  :: C(LDC, *)
-end subroutine DGEMM
-
--- src/include/dgetrf.h
+++
@@ -1,8 +0,0 @@
-pure subroutine DGETRF(M, N, A, LDA, IPIV, INFO)
-  use, intrinsic :: ISO_FORTRAN_ENV, only: rk => REAL64
-  integer, intent(in)     :: LDA, M, N
-  integer, intent(out)    :: INFO
-  integer, intent(out)    :: IPIV(*)
-  real(rk), intent(inout) :: A(LDA, *)
-end subroutine DGETRF
-
